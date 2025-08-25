54dea3a68e7e247369dd7d5febe9f27a8927762b
EzrSchwartz <Ezra.n.schwartz@gmail.com>
2025-08-25 12:29:53 -0400

    Adding EULA

---
+++ TaskR/Views/EULAView.swift
@@ -0,0 +1,34 @@
+import SwiftUI
+
+struct EULAView: View {
+    var onAgree: () -> Void
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("End User License Agreement")
+                .font(.title)
+                .bold()
+                .padding(.top)
+            
+            Text("Please read and agree to the Terms of Service and EULA before continuing.")
+                .multilineTextAlignment(.center)
+                .padding(.horizontal)
+            
+            Link("View Terms of Service", destination: URL(string: "https://yourwebsite.com/terms")!)
+                .foregroundColor(.blue)
+            
+            Spacer()
+            
+            Button(action: onAgree) {
+                Text("I Agree")
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
+            .padding(.horizontal)
+        }
+        .padding()
+    }
+}
